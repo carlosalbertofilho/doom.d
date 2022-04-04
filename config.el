8835bdd8deb35e3331f450914a5bf2ebe9ad0d97
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-04-04 12:59:08 -0300

    change to dracula's theme

--- config.el
+++ config.el
@@ -32,7 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-one)
+(setq doom-theme 'doom-dracula)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -77,11 +78,11 @@
 
 
 ;;Switch Sudo for Doas BSD config
-(defun sudo-find-file (file-name)
-  "Like find file, but opens the file as root."
-  (interactive "FSudo Find File: ")
-  (let ((tramp-file-name (concat "/doas::" (expand-file-name file-name))))
-    (find-file tramp-file-name)))
+;; (defun sudo-find-file (file-name)
+;;   "Like find file, but opens the file as root."
+;;   (interactive "FSudo Find File: ")
+;;   (let ((tramp-file-name (concat "/doas::" (expand-file-name file-name))))
+;;     (find-file tramp-file-name)))
 
 ;; Config Flyspell
 (defun flyspell-english ()
